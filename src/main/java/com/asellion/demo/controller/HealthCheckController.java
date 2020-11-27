09f680ebb095404f282135e2a565cc5ff41b5b31
sandeeppandey2 <60070709+sandeeppandey2@users.noreply.github.com>
2020-11-27 18:48:23 +0100

    Update HealthCheckController.java

--- src/main/java/com/asellion/demo/controller/HealthCheckController.java
+++ src/main/java/com/asellion/demo/controller/HealthCheckController.java
@@ -16,6 +16,7 @@ public class HealthCheckController {
 	 * Operation handling health check of API
 	 * 
 	 * @return Boolean
+	 * test
 	 */
 	@GetMapping(path = "/api/products/healthcheck.html")
 	public Boolean isHealthy() {
